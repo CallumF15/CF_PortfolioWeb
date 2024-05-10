d3f0c6433310b5b67fe95cf5e6adebcfd9477c4f
CallumF15 <callum_flannagan@msn.com>
2024-05-10 17:19:54 +0100

    t

--- server/server.js
+++ server/server.js
@@ -35,7 +35,6 @@ app.use(express.static("src"));
 
 
 app.get("/", (req, res) => {
-  // res.sendFile(path.join(process.cwd() + "/myapp/src", "public", "index.html"));
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 
