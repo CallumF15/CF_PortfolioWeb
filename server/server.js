83a1696557512adb74e40269fcb8db378a526d84
CallumF15 <callum_flannagan@msn.com>
2024-05-10 10:17:02 +0100

    test build

--- server/server.js
+++ server/server.js
@@ -8,15 +8,16 @@ var cookieParser = require('cookie-parser')
 var session = require('express-session');
 var fs = require('fs');
 
-const PORT = process.env.PORT || 5000
-
+//const PORT = process.env.PORT || 5000
 
 
 const app = express();
 app.use(cors());
 //app.use(express.static(path.join(__dirname, '../myapp/src')));
-app.use(express.static("public"));
-app.use(express.static("src"));
+// app.use(express.static("public"));
+// app.use(express.static("src"));
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
 //app.use('/bird', bird)
 
@@ -36,7 +37,7 @@ app.use(session({
 
 
 app.get("/", (req, res) => {
-  console.log(__dirname);
+  console.log("Express on Vercel");
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
