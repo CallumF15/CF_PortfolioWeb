19562416562ef11c530193db048df4605bd39379
CallumF15 <callum_flannagan@msn.com>
2024-05-10 17:10:12 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ app.use(express.static("src"));
 
 app.get("/", (req, res) => {
   // res.sendFile(path.join(process.cwd() + "/myapp/src", "public", "index.html"));
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 
 // app.route('/book')
