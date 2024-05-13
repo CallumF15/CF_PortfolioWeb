3d10151643e5953d9bd68d140baa1531c16e0e9f
CallumF15 <callum_flannagan@msn.com>
2024-05-13 13:44:43 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
-app.get("/", (req, res) => res.send("Express on Vercel"));
+const express = require("express");
+const app = express();
 
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
