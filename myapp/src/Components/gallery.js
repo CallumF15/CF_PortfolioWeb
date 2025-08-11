e2a74e528b416f5208ca3bf9399a545254abfaf0
Callum <Callum_flannagan@msn.com>
2025-08-11 18:23:25 +0100

    Reapply "V2.4"
    This reverts commit d284d1fce67ac80177db757b9f5ba21c98d8c245.

--- myapp/src/Components/gallery.js
+++ myapp/src/Components/gallery.js
@@ -10,14 +10,14 @@ const gallery = () => {
                     <div className='gallery__column'>
                         <a href="https://unsplash.com/@jeka_fe" rel="noopener noreferrer" target="_blank" className='gallery__link'>
                             <figure className="gallery__thumb">
-                                <img src="https://source.unsplash.com/_cvwXhGqG-o/300x300" alt="Portrait by Jessica Felicio" className="gallery__image" />
+                                <img src="https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png" alt="Portrait by Jessica Felicio" className="gallery__image" />
                                 <figcaption className="gallery__caption">Portrait by Jessica Felicio</figcaption>
                             </figure>
                         </a>
 
                         <a href="https://unsplash.com/@oladimeg" rel="noopener noreferrer" target="_blank" class="gallery__link">
                             <figure class="gallery__thumb">
-                                <img src="https://source.unsplash.com/AHBvAIVqk64/300x500" alt="Portrait by Oladimeji Odunsi" class="gallery__image"/>
+                                <img src="https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png" alt="Portrait by Oladimeji Odunsi" class="gallery__image"/>
                                     <figcaption class="gallery__caption">Portrait by Oladimeji Odunsi</figcaption>
                             </figure>
                         </a>
