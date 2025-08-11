ce9c49457e6aeb2ec76dc60fa8eebeb923bf2ec2
Callum <Callum_flannagan@msn.com>
2025-08-11 18:23:41 +0100

    Revert "Reapply "V2.4""
    This reverts commit e2a74e528b416f5208ca3bf9399a545254abfaf0.

--- myapp/src/Components/projects/projectDetailsAbout.js
+++ myapp/src/Components/projects/projectDetailsAbout.js
@@ -38,6 +38,12 @@ const ProjectDetailsAbout = ({ task, stringImage, btnSetState, btnState }) => {
                   {desc2}
                 </p>
 
+
+                {/* <h4 className='mt-5'> More Information </h4>
+                <p>
+                  {desc2}
+                </p> */}
+
               </div>
 
               <div className='px-3 px-sm-5 pb-3'>
@@ -46,6 +52,8 @@ const ProjectDetailsAbout = ({ task, stringImage, btnSetState, btnState }) => {
                     <>
                       <h2 className='mt-5'> Instructions </h2> <hr />
                       {parsedInstructions}
+
+                     
                     </>
                     :
                     (<h2 className='mt-5'>  </h2>)
